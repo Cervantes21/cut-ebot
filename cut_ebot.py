5bfb0a6603de4ca9b228936955db6d9b11354f38
andydollin21 <admon.owlsoul@gmail.com>
2022-09-10 04:13:19 -0500

    Primeros codigos, falta corregir y aumentar

---
+++ cut_ebot.py
@@ -0,0 +1,20 @@
+Categorias = [(loncheras), (cartucheras), (mochilas)]
+
+loncheras = [list(mini_beauty), (lonchera_labastida), (lonchera_redonda)]
+
+mini_beuty = [2 (palas_mb), 1 (f_arrastre_mb), 2 (f_cierre_mb)]
+palas_mb = int[(27) (17)]
+f_arrastre_mb = int[(50.2) (12.2)]
+f_cierre_mb = int[(30.2) (6)]
+
+lonchera_labastida = [list(2 palas_ll, 1 f_arrastre_ll, 2 f_cierre_ll 2 cartera_ll, 2 tirantes_ll)]
+palas_ll = 27.5 "x" 22
+f_arrastre_ll = 52.2 "x" 22
+f_cierre_ll = 46 "x" 7.4
+cartera_ll = 22 "x" 18.5
+tirantes_ll = 38 "x" 11.3
+
+lonchera_redonda
+cartucheras = [cartuchera_beauty_fa, cartuchera_circular, cartuchera_american]
+mochilas = [m_labastida, m_blair, m_kipling]
+print(palas)
