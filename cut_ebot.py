fa2dc3d7318daf04e845a70e86863681eecdde6b
andydollin21 <admon.owlsoul@gmail.com>
2022-09-13 23:59:32 -0500

    "Changes add"

--- cut_ebot.py
+++ cut_ebot.py
@@ -1,13 +1,13 @@
-Categorias = [(loncheras), (cartucheras), (mochilas)]
+Categorias = [loncheras, cartucheras, mochilas]
 
-loncheras = [list(mini_beauty), (lonchera_labastida), (lonchera_redonda)]
+loncheras = [mini_beauty, lonchera_labastida, lonchera_redonda]
 
-mini_beuty = [2 (palas_mb), 1 (f_arrastre_mb), 2 (f_cierre_mb)]
+mini_beuty = [2, palas_mb, 1, f_arrastre_mb, 2, f_cierre_mb]
 palas_mb = int[(27) (17)]
 f_arrastre_mb = int[(50.2) (12.2)]
 f_cierre_mb = int[(30.2) (6)]
 
-lonchera_labastida = [list(2 palas_ll, 1 f_arrastre_ll, 2 f_cierre_ll 2 cartera_ll, 2 tirantes_ll)]
+lonchera_labastida = [2 palas_ll, 1 f_arrastre_ll, 2 f_cierre_ll 2 cartera_ll, 2 tirantes_ll)]
 palas_ll = 27.5 "x" 22
 f_arrastre_ll = 52.2 "x" 22
 f_cierre_ll = 46 "x" 7.4
@@ -18,9 +18,20 @@ lonchera_redonda
 
 cartucheras = [cartuchera_beauty_fa, cartuchera_circular, cartuchera_american]
 mochilas = [m_labastida, m_blair, m_kipling]
-print(palas)
-=======
+print(mini_beuty)
+# =======
 
-cartucheras = [cartuchera_beauty_fa, cartuchera_circular, cartuchera_american]
-mochilas = [m_labastida, m_blair, m_kipling]
->>>>>>> 7ab2fed9a5ce4adad9aac90b9393073f163084ac
+# cartucheras = [cartuchera_beauty_fa, cartuchera_circular, cartuchera_american]
+# mochilas = [m_labastida, m_blair, m_kipling]
+# >>>>>>> 7ab2fed9a5ce4adad9aac90b9393073f163084ac
+# >>> modelo = input("Cual modelo quieres: ")
+# Cual modelo quieres: labastida
+# >>> modelo
+# 'labastida'
+# >>> print("Elegiste: ", modelo, " es correcto?")
+# Elegiste:  labastida  es correcto?
+# >>> print(f"Elegiste {modelo} es correcto?")
+# Elegiste labastida es correcto?
+# >>> print(f"Elegiste: {modelo} es correcto?")
+# Elegiste: labastida es correcto?
+# >>> cantidad = int(input("Cuántas mochilas quieres: "))
