aa1006625f90b658fabc42c5c28f9201c8f5fb38
cervantes21 <admon.owlsoul@gmail.com>
2022-12-23 16:49:55 -0600

    changes

--- cut_ebot.py
+++ cut_ebot.py
@@ -1,9 +1,9 @@
 def run():
     pass
-def mochilas():
-    numero = ()
-    color = ()
-    respuesta = opcion
+def mochilas(numero,color):
+    numero = int()
+    color = str()
+    
 def loncheras():
     pass
 def cartucheras():
@@ -13,19 +13,22 @@ opcion = int(input(f'Escoge una categoria \n 1. mochilas \n 2. loncheras \n 3. c
 
 if opcion == 1:
     print('1. Mochilas')
-    numero = int(input('* Cuantas mochilas: '))
+    numero = int(input('* Cuantas Mochilas: '))
     color = str(input('* Digite un color: '))
     mochilas(numero,color)
+    respuesta = (f'Necesitas {numero*4} de palas y {numero*2} tirantes color:{color}')
+    print(respuesta)
+    
 elif opcion == 2:
     print('2. Loncheras')
-    numero = int(input('* Cuantas mochilas: '))
+    numero = int(input('* Cuantas Loncheras: '))
     color = str(input('* Digite un color: '))
     loncheras(numero,color)
 elif opcion == 3:
     print('3. Cartucheras')
-    numero = int(input('* Cuantas mochilas: '))
+    numero = int(input('* Cuantas Cartucheras: '))
     color = str(input('Digite un color: '))
-    Cartucheras(numero,color)
+    cartucheras(numero,color)
 else:
     print('Opcion no valida')
 
--- test_cut_ebot.py
+++ test_cut_ebot.py
@@ -1,99 +1,44 @@
-Categorias = [loncheras, cartucheras, mochilas]
+def categoria(opcion):
+    opcion = int(input(f'Escoge una categoria \n 1. mochilas \n 2. loncheras \n 3. cartucheras \n'))
 
-loncheras = [mini_beauty, lonchera_labastida, lonchera_redonda]
+    if opcion == 1:
+        print('1. Mochilas')
+    elif opcion == 2:
+        print('2. Loncheras')
+    elif opcion == 3:
+        print('3. Cartucheras')
+    else:
+        print('Opcion no valida')
 
-mini_beuty = [2, palas_mb, 1, f_arrastre_mb, 2, f_cierre_mb]
-palas_mb = int[(27) (17)]
-f_arrastre_mb = int[(50.2) (12.2)]
-f_cierre_mb = int[(30.2) (6)]
+    def n_pzas(opcion,numero):
+        if opcion== 1:
+            numero = int(input('* Cuantas mochilas: '))
+        elif opcion==2:
+            numero = int(input('* Cuantas loncheras: '))
+        elif opcion==3:
+            numero = int(input('* Cuantas Cartucheras: '))
+        else:
+            print('Opcion no valida')
+        return numero
 
-lonchera_labastida = [2 palas_ll, 1 f_arrastre_ll, 2 f_cierre_ll 2 cartera_ll, 2 tirantes_ll)]
-palas_ll = 27.5 "x" 22
-f_arrastre_ll = 52.2 "x" 22
-f_cierre_ll = 46 "x" 7.4
-cartera_ll = 22 "x" 18.5
-tirantes_ll = 38 "x" 11.3
+    def choose_color(color):
+        colors = [
+            'Azul',
+            'Rojo',
+            'A. Marino',
+            'Negro',
+            'Guinda',
+            'Amarillo'
+        ]
+        for c in colors:
+            print(c)
+        color = str(input('* Digite un color: '))
+        return color
 
-lonchera_redonda
-
-cartucheras = [cartuchera_beauty_fa, cartuchera_circular, cartuchera_american]
-mochilas = [m_labastida, m_blair, m_kipling]
-print(mini_beuty)
-# =======
-
-# >>>>>>> 7ab2fed9a5ce4adad9aac90b9393073f163084ac
-# >>> modelo = input("Cual modelo quieres: ")
-# Cual modelo quieres: labastida
-# >>> modelo
-# 'labastida'
-# >>> print("Elegiste: ", modelo, " es correcto?")
-# Elegiste:  labastida  es correcto?
-# >>> print(f"Elegiste {modelo} es correcto?")
-# Elegiste labastida es correcto?
-# >>> print(f"Elegiste: {modelo} es correcto?")
-# Elegiste: labastida es correcto?
-# >>> cantidad = int(input("Cuántas mochilas quieres: "))
-# estudiantes = {
-#     'mexico': 10,
-#     'colombia': 15,
-#     'puerto_rico': 4,
-# }
-
-# for pais in estudiantes:
-#     ...
-
-# for pais in estudiantes.keys():
-#     ...
-
-# for numero_de_estudiantes in estudiantes.values():
-#     ...
-
-# for pais, numero_de_estudiantes in estudiantes.items():
-#     ...
-
-class categorias (mochilas, loncheras, cartucheras):
-    mochilas = [m_labastida, m_blair, m_kipling]
-    loncheras = [mini_beauty, l_labastida, l_redonda]
-    cartucheras = [c_beauty_fa, c_circular, c_american]
-    
-    def mochilas(numero):
-        m_labastida = 0
-        m_blair = 0
-        m_kipling = 0
-        
-    def loncheras(numero):
-        mini_beauty = 0
-        l_labastida = 0
-        l_redonda = 0
-    
-    def cartucheras(numero):
-        c_beauty_fa = 0
-        c_circular = 0
-        c_american = 0
-        
-opcion = int(input(f'Escoge una categoria \n 1. mochilas \n 2. loncheras \n 3. cartucheras \n'))
-
-if opcion == 1:
-    print('1. Mochilas')
-    numero = int(input('* Cuantas mochilas: '))
-    color = str(input('* Digite un color: '))
-    mochilas(numero,color)
-elif opcion == 2:
-    print('2. Loncheras')
-    numero = int(input('* Cuantas mochilas: '))
-    color = str(input('* Digite un color: '))
-    loncheras(numero,color)
-elif opcion == 3:
-    print('3. Cartucheras')
-    numero = int(input('* Cuantas mochilas: '))
-    color = str(input('Digite un color: '))
-    Cartucheras(numero,color)
-else:
-    print('Opcion no valida')
+def choose_material(material):
 
 def run():
     pass
 
-
-if __name__ == "__main__":
+if __name__ == '_main_':
     run()
