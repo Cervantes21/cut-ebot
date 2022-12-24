d59eed925e61495f26f9292eae596c173541b926
andydollin-pc <cervantes21@DESKTOP-Q8PT42K>
2022-12-24 16:32:03 -0600

    changes to inputs.py

--- inputs.py
+++ inputs.py
@@ -60,3 +60,26 @@ elif opcion == 3:
     Cartucheras(numero,parametro_epsilon)
 else:
     print('Opcion no valida')
+    
+# Imprimimos el menú en pantalla
+print("""
+    1) Herramientas       3) Temperatura
+    2) Calculadora        4) Mas...
+    """)
+
+# Leemos lo que ingresa el usuario
+eligio=input("-Selecciona algo :")
+
+# Según lo que ingresó, código diferente
+if eligio=="1":
+    print("Listamos otras herramientas")
+elif eligio=="2":
+    x = 3
+    y = 5
+    print("x * y = ", x * y)
+elif eligio=="3":
+    print("Creo que hace frío")
+elif eligio=="4":
+    print("otra opción")
+else:
+    print("Opción no válida")
